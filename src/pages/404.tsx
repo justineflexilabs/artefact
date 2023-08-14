f5d3023a14e511aee062c3681447947e8db5bff0
Justine Espinosa <justine.espinosa@flexilabs.co>
2023-08-14 14:19:35 +0800

    Added 404 and 500 pages

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,27 @@
 import React from 'react';
 
+import { Button, Card, CardBody, Flex, Text } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+
 export default function NotFound() {
-  return <div>Page is not found!</div>;
+  const router = useRouter();
+
+  return (
+    <Flex alignItems={'center'} justifyContent={'center'} height={'100vh'}>
+      <Card>
+        <CardBody
+          display={'flex'}
+          alignItems={'center'}
+          justifyContent={'center'}
+          flexDirection={'column'}
+          p={10}
+        >
+          <Text>Sorry, Page is not found.</Text>
+          <Button onClick={() => router.back()} variant={'primary'} mt={5}>
+            Go Back
+          </Button>
+        </CardBody>
+      </Card>
+    </Flex>
+  );
 }
--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,5 +1,27 @@
 import React from 'react';
 
+import { Button, Card, CardBody, Flex, Text } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+
 export default function ServerErrorPage() {
-  return <div>Unexpected 500 Error on the Server.</div>;
+  const router = useRouter();
+
+  return (
+    <Flex alignItems={'center'} justifyContent={'center'} height={'100vh'}>
+      <Card>
+        <CardBody
+          display={'flex'}
+          alignItems={'center'}
+          justifyContent={'center'}
+          flexDirection={'column'}
+          p={10}
+        >
+          <Text>Sorry, something went wrong.</Text>
+          <Button onClick={() => router.back()} variant={'primary'} mt={5}>
+            Go Back
+          </Button>
+        </CardBody>
+      </Card>
+    </Flex>
+  );
 }
